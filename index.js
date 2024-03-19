971a8b877720d2691035c402092a409d1f187b88
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-19 17:23:30 -0500

    add: Object basics

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const daniel = {
 	name: 'Daniel',
 	age: 20,
 	courses: ['HTML', 'CSS', 'JavaScript'],
+
+	passCourse(newCourse) {
+		this.courses.push(newCourse);
+	},
 };
+
+console.log(daniel.name);
+console.log(daniel.courses);
+
+daniel.passCourse('Python');
+
+console.log(daniel.courses);
